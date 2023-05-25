83b204a3a9986322e76e7aaaee004daf3d7cf740
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-05-25 08:52:28 -0500

    feat: stubbed the cart class

--- packages/cart/index.js
+++ packages/cart/index.js
@@ -1,3 +1,79 @@
+class Cart {
+    constructor({}){
+
+    }
+
+    addProduct(product){
+        return true;
+    }
+
+    removeProduct(product){
+        return true;
+    }
+
+    increaseQuantity(product){
+        return true;
+    }
+
+    decreaseQuantity(product){
+        return true;
+    }
+
+    updateQuantity(product, quantity){ 
+        return true;
+    }
+
+    addCoupon(coupon){
+        return true;
+    }
+
+    clear(){
+        return true;
+    }
+
+    getProducts(){
+        return [];
+    }
+
+    calculateTotal(){
+        return 0;
+    }
+
+    calculateTotalWithTax(){
+        return 0;
+    }
+
+    calculateTax(){
+        return 0;
+    }
+
+    calculateShipping(){
+        return 0;
+    }
+
+    calculateDiscount(){
+        return 0;
+    }
+
+    calculateSubtotal(){
+        return 0;
+    }
+
+    calculateTotalItems(){
+        return 0;
+    }
+
+    calculateTotalUniqueItems(){
+        return 0;
+    }
+
+    calculateLineItemTotal(product){
+        return 0;
+    }
+
+}
+
+
 module.exports = {
-    
+    Cart
 }
