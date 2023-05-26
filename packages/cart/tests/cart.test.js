b5a4a7e3e97437c77926d9f633debd80ac65f842
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-05-25 23:56:45 -0500

    feat: writing a failing test

---
+++ packages/cart/tests/cart.test.js
@@ -0,0 +1,103 @@
+const assert = require('assert');
+
+const { expect } = require('chai');
+
+const { Cart } = require('../index.js')
+describe('cart', () => {
+
+    it('should add a product to the cart', () => {
+        // step1: create a new cart
+        const cart = new Cart();
+        // step2: add a product to the cart
+        cart.addProduct({ id: 1, name: 'product1', price: 100, quantity: 1 });
+        // step3: check if the product is in the cart
+        expect(cart.products).to.have.lengthOf(1);
+        expect(cart.products[0].id).to.equal(1);
+        expect(cart.products[0].name).to.equal('product1');
+        expect(cart.products[0].price).to.equal(100);
+        expect(cart.products[0].quantity).to.equal(1);
+    })
+
+    it('should remove a product from the cart', () => {
+        expect(false).to.be.true;
+
+    })
+
+    it('should increase the quantity of a product in the cart', () => {
+        expect(false).to.be.true;
+
+    })
+
+    it('should not add a product to the cart if the quantity is less than or equal to zero', () => {
+        expect(false).to.be.true;
+
+    })
+
+    it('should not remove a product from the cart if it is not in the cart', () => {
+        expect(false).to.be.true;
+
+    })
+
+    it('should not remove a product from the cart if the quantity is greater than the quantity in the cart', () => {
+        expect(false).to.be.true;
+    })
+
+    it('should remove a product from the cart if the quantity is less than or equal to zero', () => {
+        expect(false).to.be.true;
+
+    })
+
+    it('should update the quantity of a product in the cart', () => {
+        expect(false).to.be.true;
+
+    })
+
+    it('should update the quantity of a product in the cart to zero', () => {
+        expect(false).to.be.true;
+
+    })
+
+    it('should not update the quantity of a product in the cart to a negative number', () => {
+        expect(false).to.be.true;
+
+    })
+
+    it('should not update the quantity of a product in the cart to a non-integer', () => {
+        expect(false).to.be.true;
+
+    })
+
+    it('should clear the cart', () => {
+        expect(false).to.be.true;
+
+    })
+
+    it('should calculate the line total of a product in the cart', () => {
+        expect(false).to.be.true;
+    })
+
+    it('should calculate the total price of the cart', () => {
+        expect(false).to.be.true;
+
+    })
+
+    it('should calculate the total price of the cart with a discount', () => {
+        expect(false).to.be.true;
+
+    })
+
+    it('should calculate the total price of the cart with a discount and shipping', () => {
+        expect(false).to.be.true;
+    })
+
+    it('should calculate the total price of the cart with a discount and shipping and taxes', () => {
+        expect(false).to.be.true;
+
+    })
+
+    it('should calculate the total price of the cart with a discount and shipping and taxes and a coupon', () => {
+        expect(false).to.be.true;
+
+    })
+
+})
