9307dc70d0f57529a839e7f826c80dcef1f5eeb3
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-05-26 00:42:09 -0500

    feat: writing a failing test to remove a product from the cart if the quantity is less than or equal to zero

--- packages/cart/tests/cart.test.js
+++ packages/cart/tests/cart.test.js
@@ -74,7 +74,14 @@ describe('cart', () => {
     })
 
     it('should remove a product from the cart if the quantity is less than or equal to zero', () => {
-        expect(false).to.be.true;
+        // step1: create a new cart
+        const cart = new Cart();
+        // step2: add a product to the cart
+        cart.addProduct({ id: 1, name: 'product1', price: 100, quantity: 1 });
+        // step3: remove the product from the cart
+        cart.removeProduct({ id: 1, name: 'product1', price: 100, quantity: -2 });
+        // step4: check if the product is in the cart
+        expect(cart.products).to.have.lengthOf(0);
 
     })
 
